aba4de6337739695ab6694b1eb36562c83850085
CoraliaGabrielli <130628214+CoraliaGabrielli@users.noreply.github.com>
2023-05-29 23:55:09 -0300

    Add files via upload

---
+++ js/javascript.js
@@ -0,0 +1,10 @@
+let edadUsuario = prompt ("Ingrese su edad");
+
+function edad (edad){
+    let edadUsuario;
+    if (edad < 18){
+        alert ("Usted esta ingresando bajo su responsabilidad");
+    }
+}
+edad (edadUsuario);
+
