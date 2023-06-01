050c153a6d67309c6d79c29a2686cd17c6114fb0
CoraliaGabrielli <130628214+CoraliaGabrielli@users.noreply.github.com>
2023-05-31 21:16:41 -0300

    Add files via upload

--- js/javascript.js
+++ js/javascript.js
@@ -1,10 +1,21 @@
 let edadUsuario = prompt ("Ingrese su edad");
-
 function edad (edad){
     let edadUsuario;
-    if (edad < 18){
+    if (edad >= 18){
+        alert ("Bienvenido");
+    } else {            
         alert ("Usted esta ingresando bajo su responsabilidad");
     }
 }
 edad (edadUsuario);
 
+
+
+function ubicacion (){    
+    let sanNicolas = confirm("Usted reside en San Nicolás?");    
+    if (sanNicolas == true){
+        alert ("El asesoramiento es gratis");
+    } else {
+        alert ("El asesoramiento tiene un costo de $800");
+    }
+}
